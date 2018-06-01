cb920cc84feac86bdeb93a33337594a81affc23e
LennyBalanetaNote <=>
2018-06-01 14:09:49 -0300

    model v1

---
+++ src/main/java/br/com/academiadev/reembolsoazul/model/RefundStatus.java
@@ -0,0 +1,31 @@
+package br.com.academiadev.reembolsoazul.model;
+
+public enum RefundStatus {
+	ACCEPTED("ACCEPTED", "Accepted"),//
+	REFUSED("REFUSED", "Refused"),//
+	WAITING("WAITING", "Waiting");
+	
+	private String id;
+	private String name;
+	
+	private RefundStatus(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
