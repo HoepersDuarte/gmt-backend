a7ca0c9378f6cfdbe394f73f7f5caba9cdede4a7
LennyBalanetaNote <=>
2018-06-16 17:16:33 -0300

    utilizacao de roles

--- src/main/java/br/com/academiadev/reembolsoazul/model/UserType.java
+++ src/main/java/br/com/academiadev/reembolsoazul/model/UserType.java
@@ -1,5 +1,12 @@
 package br.com.academiadev.reembolsoazul.model;
 
-public enum UserType {
+import org.springframework.security.core.GrantedAuthority;
+
+public enum UserType implements GrantedAuthority{
 	ADMIN, COMMONUSER;
+
+	@Override
+	public String getAuthority() {
+		return this.toString();
+	}
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,5 @@
-INSERT INTO AUTH (id, name) VALUES (1, 'ROLE_USER');
-INSERT INTO AUTH (id, name) VALUES (2, 'ROLE_ADMIN');
-
 INSERT INTO COMPANY (id, company_admin_code, company_user_code, name) VALUES (10, 'admin-code', 'user-code', 'Company name');
 
 INSERT INTO USERI (id, email,  name, password, user_type, company_id, last_password_change) VALUES (2, 'admin', 'Admin name', '$2a$10$nW9TstAiJ0Eiewy3wJ4pk.WjM4lQFj41GakDNU4MqHdvAfC1fz9YG', 1, 10, '2017-10-01');
+
+INSERT INTO USERI (id, email,  name, password, user_type, company_id, last_password_change) VALUES (3, 'user', 'User name', '$2a$10$Y6JAKuyBHEjg..ybDiJ0HucjW0ZVLZj2f7/7ij350L0lGQUfQDk0C', 0, 10, '2017-10-01');
