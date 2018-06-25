f340d7edf06db775d652d17a37557e70f2bdf116
Marcos Balatka <m.balatka5@gmail.com>
2018-06-25 17:31:46 -0300

    get company utilizando o token

--- src/main/java/br/com/academiadev/reembolsoazul/controller/CompanyController.java
+++ src/main/java/br/com/academiadev/reembolsoazul/controller/CompanyController.java
@@ -3,14 +3,20 @@ package br.com.academiadev.reembolsoazul.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.academiadev.reembolsoazul.dto.CompanyRegisterDTO;
+import br.com.academiadev.reembolsoazul.dto.CompanyViewDTO;
+import br.com.academiadev.reembolsoazul.exception.UserNotFoundException;
 import br.com.academiadev.reembolsoazul.service.CompanyService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
@@ -29,6 +35,17 @@ public class CompanyController {
 	public ResponseEntity<CompanyRegisterDTO> register(@RequestBody CompanyRegisterDTO companyRegisterDTO) {
 		companyService.register(companyRegisterDTO);
 		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
+	@ApiOperation(value = "Retorna a empresa referente ao admin logado", response = CompanyViewDTO.class)
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Empresa retornada com sucesso") })
+	@ApiImplicitParams({ //
+		@ApiImplicitParam(name = "Authorization", value = "Authorization token", required = true, dataType = "string", paramType = "header") //
+	})
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@GetMapping("/")
+	public ResponseEntity<CompanyViewDTO> getCompany() throws UserNotFoundException {
+		return new ResponseEntity<>(companyService.getCompany(), HttpStatus.OK);
 	}
 
 }
--- src/main/java/br/com/academiadev/reembolsoazul/service/CompanyService.java
+++ src/main/java/br/com/academiadev/reembolsoazul/service/CompanyService.java
@@ -9,7 +9,9 @@ import org.springframework.stereotype.Service;
 import br.com.academiadev.reembolsoazul.converter.CompanyViewConverter;
 import br.com.academiadev.reembolsoazul.dto.CompanyRegisterDTO;
 import br.com.academiadev.reembolsoazul.dto.CompanyViewDTO;
+import br.com.academiadev.reembolsoazul.exception.UserNotFoundException;
 import br.com.academiadev.reembolsoazul.model.Company;
+import br.com.academiadev.reembolsoazul.model.User;
 import br.com.academiadev.reembolsoazul.model.UserType;
 import br.com.academiadev.reembolsoazul.repository.CompanyRepository;
 import br.com.academiadev.reembolsoazul.util.CompanyTokenHelper;
@@ -23,6 +25,9 @@ public class CompanyService {
 	@Autowired
 	private CompanyRepository companyRepository;
 
+	@Autowired
+	private UserService userService;
+	
 	public void register(CompanyRegisterDTO companyRegisterDTO) {
 		Company company = new Company();
 
@@ -37,6 +42,11 @@ public class CompanyService {
 		return Util.toList(companyRepository.findAll()).stream().map(e -> {
 			return companyViewConverter.toDTO(e);
 		}).collect(Collectors.toList());
+	}
+	
+	public CompanyViewDTO getCompany() throws UserNotFoundException {
+		User user = userService.findUserByToken();
+		return companyViewConverter.toDTO(user.getCompany());
 	}
 
 	private void setAdminAndUserCodes(String companyName, Company company) {
